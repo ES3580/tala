f912e66ebe638bc5eb3522914074e510500f48e6
ES3580 <esteidl@live.com>
2020-09-17 17:39:05 -0600

    Added Functionality to customize w/b list port ranges

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -3,7 +3,7 @@
 mod channel;
 
 fn main() {
-    let c = channel::Channel { mode: channel::Hype::BLACKLIST, /*name: String::from("c1"),*/ ..Default::default()};
+    let c = channel::Channel { mode: channel::ChannelMode::BLACKLIST, /*name: String::from("c1"),*/ ..Default::default()};
     println!("Hello, world!");
     println!("{}", c.to_string());
 }
