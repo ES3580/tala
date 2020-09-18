d2231c2afbb6e84c1d323e3ce28147c5124bd29f
ES3580 <esteidl@live.com>
2020-09-17 18:18:19 -0600

    Added basic functionality for the Data class. It is implemented with a queue of strings since the ZMQ library works with strings

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -3,7 +3,8 @@
 mod channel;
 
 fn main() {
-    let c = channel::Channel { mode: channel::ChannelMode::BLACKLIST, /*name: String::from("c1"),*/ ..Default::default()};
+    let mut c = channel::Channel { mode: channel::ChannelMode::BLACKLIST, /*name: String::from("c1"),*/ ..Default::default()};
+    c.add(44);
     println!("Hello, world!");
     println!("{}", c.to_string());
 }
